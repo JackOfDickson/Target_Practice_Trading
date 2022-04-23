c808481d2e9dc32b286c4f6e1f66da2249fe46a0
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-04-23 22:19:09 +0100

    quick change

--- client/src/components/Stock.js
+++ client/src/components/Stock.js
@@ -13,28 +13,25 @@ const Stock = ({coin, addCrypto, cashWallet}) => {
 
     const handleAmountChange = (event)=>
     {
-        if (event.target.value > cashWallet)
+        if (event.target.value <= cashWallet)
         {
-
-        }
-        else{
-        setAmountInput(event.target.value)
+            setAmountInput(event.target.value)
         }
     }
-
     const priceTo2Decimals = parseFloat(coin.priceUsd).toFixed(2); // Show price to two decimal places
-    // const cryptoPrice = parseFloat(priceTo2Decimals).toLocaleString("en-US"); // Separate large numbers with commas
+    const cryptoPrice = parseFloat(priceTo2Decimals).toLocaleString("en-US"); // Separate large numbers with commas
 
     return (
     <tr>
         <td>{coin.name}</td>
         <td>{coin.symbol}</td> 
-        <td>Price: ${priceTo2Decimals}</td>
+        <td>Price: ${cryptoPrice}</td>
         <td>
             <form onSubmit={handleBuySubmit}>
                 <button type='submit'>Buy Coin</button>
-                <input type='number' value={amountInput} min="0" onChange={handleAmountChange} required></input>
+                <input type='number' value={amountInput} min='0' onChange={handleAmountChange} required></input>
             </form>
+
         </td>
     </tr>
     )
