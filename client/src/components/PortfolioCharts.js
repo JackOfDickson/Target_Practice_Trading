711975d3650d0c68aadb137a415917db5572c8a2
Jack <jackowendickson@gmail.com>
2022-04-25 15:25:41 +0100

    chart showing

--- client/src/components/PortfolioCharts.js
+++ client/src/components/PortfolioCharts.js
@@ -1,26 +1,34 @@
-import { Charts } from "react-google-charts"
+import { Chart } from "react-google-charts"
 
 const PortfolioCharts = ({portfolio, cash}) => {
 
 
+    
+    // const totalInvested= portfolio.reduce((previous, current) => previous + current.investment, 0 );
 
-    // const investmentAmountArray = portfolio.map(coin_investments => coin_investments.investment)
-    // const totalAmount = investmentAmountArray.reduce((previous, current) => previous + current, 0)
-    
-    const totalInvested= portfolio.reduce((previous, current) => previous + current.investment, 0 )
-    
-    // const totalAmount = investmentAmountArray.reduce()
+    // const totalEquity = totalInvested + cash;
+
+    const pieChartData = [["Coin", "Cash invest"], ["Cash USD", cash]];
+
+    portfolio.map(investment => {
+        const currentInvestment = [investment.coin.name, investment.investment]
+        pieChartData.push(currentInvestment)
+    })
+
 
     return (
         <>
             <p>Chart</p>
-            {/* <Chart
-  chartType="ScatterChart"
-  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-  width="100%"
-  height="400px"
-  legendToggle
-/> */}
+
+            
+            <Chart
+                chartType="PieChart"
+                data={pieChartData}
+                // options = "my investment"
+                width="100%"
+                height="400px"
+                legendToggle
+/>
         </>
     )
 };
