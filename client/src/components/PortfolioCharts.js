0706d7fd441efe1a14d5cc08cfa05cd96479f49a
Jack <jackowendickson@gmail.com>
2022-04-25 15:01:59 +0100

    getting the total amounts invested

--- client/src/components/PortfolioCharts.js
+++ client/src/components/PortfolioCharts.js
@@ -1,10 +1,26 @@
-import { Chart } from "react-google-charts";
+import { Charts } from "react-google-charts"
 
-const PortfolioCharts = () => {
+const PortfolioCharts = ({portfolio}) => {
+
+
+
+    // const investmentAmountArray = portfolio.map(coin_investments => coin_investments.investment)
+    // const totalAmount = investmentAmountArray.reduce((previous, current) => previous + current, 0)
+    
+    const totalInvested= portfolio.reduce((previous, current) => previous + current.investment, 0 )
+    
+    // const totalAmount = investmentAmountArray.reduce()
 
     return (
         <>
             <p>Chart</p>
+            {/* <Chart
+  chartType="ScatterChart"
+  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+  width="100%"
+  height="400px"
+  legendToggle
+/> */}
         </>
     )
 };
