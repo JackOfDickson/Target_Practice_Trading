d7917489beea4a7e8bb96b275b14b6c0933b6545
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-04-26 20:00:35 +0100

    Successful build of bitcoin analysis chart

--- client/src/components/analysis.js
+++ client/src/components/analysis.js
@@ -1,4 +1,6 @@
 import { useState, useEffect} from 'react';
+import './css/Analysis.css';
+import { Chart } from "react-google-charts";
 
 const Analysis = () => {
 
@@ -14,27 +16,59 @@ const Analysis = () => {
     const getHistory = function () { 
         fetch ('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
             .then (response => response.json())
-            .then (result => setAnalysis (result.data));
+            .then (result => setAnalysis (result.data.reverse().slice(0,200))); 
+            // Reverse function used as API places new data at end
+            // Slice function used to limit array to a defined number of items
     };
 
     // Format API data
-    let dataMap;
+    let dataMap=[];
     analysis.map( crypto => {
-
-        let dateTemp = crypto.date;
-        // const coinInfo=[crypto.priceUsd, crypto.sell_price];
-        // data.push(coinInfo);
-        // setAnalysis
+        const initialDate = crypto.date.split("T") // Split date stamp at timeline
+        const dateTemp = initialDate[0]; // Capture date part of date stamp
+        const formattedDate = new Date(dateTemp);
+        const priceTemp=parseFloat(crypto.priceUsd);
+        const coinInfo = {
+            date: formattedDate,
+            price: priceTemp
+        } ;
+        dataMap.push(coinInfo);
+ 
     });
+
+    // Build Chart Data
+
+    let data = [["Year", "Price"]];
+
+    dataMap.map( crypto => {
+        const coinInfo=[crypto.date, crypto.price];
+        data.push(coinInfo);
+    });
+
+    console.log(data)
+
+    const options = {
+        title: "Bitcoin Price in USD",
+        curveType: "function",
+        legend: { position: "bottom" },
+    };
 
 
     return (
+        <div id="analysis">
+            <h3>Bitcoin Analysis <i class="fa-solid fa-chart-line"></i></h3>
+            <p>Bitcoin remains the most influential cryptocurrency in the market.
+            When it goes up or down, the rest of the market tends to follow its lead.</p>
 
-        <>
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={data}
+                options={options}
+            />
 
-
-
-        </>
+        </div>
     )
 
 };
