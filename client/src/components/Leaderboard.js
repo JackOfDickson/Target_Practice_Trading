bca20122d5c5c3284a7c2257f6da5208146d3845
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2022-04-26 15:22:15 +0100

    Feature/multiplayer (#29)
    * added login
    
    * fixed the issue with updating page after transaction
    
    * added log out feature
    
    * moved logout button to the sied
    
    * leaderboard added
    
    * polished leaderboard
    
    Co-authored-by: Matt-Matthaiou <mat.matthaiou@gmail.com>

---
+++ client/src/components/Leaderboard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { LBC } from "./Calculator";
+
+const Leaderboard = ({allUsers, cryptos, activeUser})=>
+{
+
+const userRanks = allUsers.map(user=>
+    {
+        return LBC(user, cryptos)
+    })
+const sortRanks = userRanks.sort((a,b)=>
+{
+    
+    return b.equity - a.equity
+})
+
+const leaderBoard = sortRanks.map(rank=>
+    {
+        if(rank.name === activeUser.name)
+        {
+            return <li><strong >{rank.name}  ${rank.equity}<span>&#8592;</span></strong></li>
+        }
+        else
+        {
+            return <li>{rank.name}  ${rank.equity}</li>
+        }
+    })
+
+
+    return(
+        <ol>
+            {leaderBoard}
+        </ol>
+    )
+}
+
+export default Leaderboard;
