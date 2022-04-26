88b1ca368a07dbbd5b786db187da89f7310c85ce
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-04-26 14:31:13 +0100

    Combined crypto purchases when listing value of coins

--- client/src/components/UserStats.js
+++ client/src/components/UserStats.js
@@ -23,70 +23,70 @@ const UserStats = ({activeUser, investmentValue}) => {
         )
     });
 
-    // map investment portfolio
-    let investmentPortfolio;
-    let investmentCalculation = 0;
-
-    if (investmentValue) { // check to see investments exist
-
-    investmentPortfolio = investmentValue.map( crypto => {
-
-        investmentCalculation+=crypto.sell_price;
-
-        const formatSellPrice = parseFloat(crypto.sell_price).toFixed(2); // format sell price to two decimal places
-        const sellPrice = parseFloat(formatSellPrice).toLocaleString("en-US"); // Separate large numbers with commas
-
-
-        return (
-            <>
-                <br />• {crypto.coin} investment is now worth ${sellPrice}
-            </>
-            )        
-    });
-
-    }  
-
-    // Calculation and formatting for cash and crypto balances
-    const formatInvestmentTotalValue = parseFloat(investmentCalculation).toFixed(2); // format sell price to two decimal places
-    const investmentTotalValue = parseFloat(formatInvestmentTotalValue).toLocaleString("en-US"); // Separate large numbers with commas
-
-    const workingBalance = parseFloat(activeUser.cash) + parseFloat(investmentCalculation); // Calculate total value of portfolio
-    const formatWorkingBalance = parseFloat(workingBalance).toFixed(2); // format sell price to two decimal places
-    const equityBalance = parseFloat(formatWorkingBalance).toLocaleString("en-US"); // Separate large numbers with commas
-
-    // Data for User stats chart
-
     // Create investment array showing coin name and coin price
 
     let investmentArray=[];
     if (investmentValue) { // check to see if investments exist
         investmentValue.map( crypto => {
             const coinInfo={
-                name: crypto.coin, 
-                price: crypto.sell_price 
+                name: crypto.coin, // name of crypto
+                price: crypto.sell_price // sell price of crypto
             };
             investmentArray.push(coinInfo);
         });
     } // end if
 
 
-
-console.log(investmentArray)
-
-for (let i = 0; i < investmentArray.length; ++i) {
+// Function to combine current value of crypto purchases
+for (let i = 0; i < investmentArray.length; ++i) { // Run through each item in investmentArray
    
-    for (let unique = 0; unique < investmentArray.length; unique++) {
+    for (let unique = 0; unique < investmentArray.length; unique++) { // Second for loop to find unique keys
         
-        if (i !== unique && investmentArray[i].name === investmentArray[unique].name) {
-            investmentArray[i].price+=investmentArray[unique].price;
-            investmentArray.splice(unique, 1); 
-    }
-        
-    }
+        // if cryptocurrency is found when looping round the second time
+        if (i !== unique && investmentArray[i].name === investmentArray[unique].name) { 
+            investmentArray[i].price+=investmentArray[unique].price; // Add value to first listing of cryptocurrency
+            investmentArray.splice(unique, 1); // Remove duplicate entry from investmentArray
+        } // end if
+    } // end of second for loop
+} // end of first for loop
 
-}
+
+// map investment portfolio for calculating value of crypto portfolio
+    let investmentPortfolio;
+    let investmentCalculation = 0;
+
+   if (investmentValue) { // check to see investments exist
+
+    investmentPortfolio = investmentArray.map( crypto => {
+
+        investmentCalculation+=crypto.price;
+
+       const formatSellPrice = parseFloat(crypto.price).toFixed(2); // format sell price to two decimal places
+       const sellPrice = parseFloat(formatSellPrice).toLocaleString("en-US"); // Separate large numbers with commas
+
+
+        return (
+            <>
+                <br />• {crypto.name} investment is now worth ${sellPrice}
+            </>
+            )        
+    });
+
+    }  
+// End of mapping investment portfolio
+
+
+   // Calculation and formatting for cash and crypto balances
+   const formatInvestmentTotalValue = parseFloat(investmentCalculation).toFixed(2); // format sell price to two decimal places
+   const investmentTotalValue = parseFloat(formatInvestmentTotalValue).toLocaleString("en-US"); // Separate large numbers with commas
+
+   const workingBalance = parseFloat(activeUser.cash) + parseFloat(investmentCalculation); // Calculate total value of portfolio
+   const formatWorkingBalance = parseFloat(workingBalance).toFixed(2); // format sell price to two decimal places
+   const equityBalance = parseFloat(formatWorkingBalance).toLocaleString("en-US"); // Separate large numbers with commas
+
+
         
-console.log(investmentArray);
+    // Data for User stats chart
 
     let data = [["Crypto", "USD Value"]];
 
@@ -95,7 +95,6 @@ console.log(investmentArray);
         data.push(coinInfo);
     });
 
-    console.log(data)
 
     const options = {
         title: "Value of Invested Cryptos in USD",
@@ -104,6 +103,8 @@ console.log(investmentArray);
         bar: { groupWidth: "95%" },
         legend: { position: "none" },
         };
+
+    // End of chart data building section
 
     return (
 
