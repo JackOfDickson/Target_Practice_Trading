a7ae4b517f583537538b58a39395b28b0376ac9d
Jack Dickson <jackowendickson@gmail.com>
2022-04-27 11:54:45 +0100

    added text to how to play and styled graphs in the market section

--- client/src/designComponents/HowToPlay.js
+++ client/src/designComponents/HowToPlay.js
@@ -7,7 +7,7 @@ const HowToPlay = () => {
             <div class="howToContainer">
                 <header>
                     <h2 class="howToTitle">How to Play Target Practice Trader</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>First have a look at the market and check the latest changes then scroll down and proceed to buy some cryptocurrencies! To sell your purchased coins just simply click the "sell coin" button on the investment. Will your crypto go to the moon or did you buy a $hit coin? Checkout the leaderboard to see how you are doing against your friends!</p>
                 </header>
 
             </div>
